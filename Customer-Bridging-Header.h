4718af692dff6085b0e3df8b86f5862b59ea5022
Tomas <shellvon21@gmail.com>
2017-07-19 21:31:57 +0800

    scale done

---
+++ Customer-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Customer-Bridging-Header.h
+//  momoCustomer
+//
+//  Created by ssstand on 17/07/2017.
+//  Copyright © 2017 MomoDidi. All rights reserved.
+//
+
+#ifndef Customer_Bridging_Header_h
+#define Customer_Bridging_Header_h
+
+
+#endif /* Customer_Bridging_Header_h */
---
+++ Pods/Target Support Files/Pods-momoCustomer/Pods-momoCustomer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_momoCustomer : NSObject
+@end
+@implementation PodsDummy_Pods_momoCustomer
+@end
